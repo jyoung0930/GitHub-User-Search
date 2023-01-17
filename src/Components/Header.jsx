556ebc1ad4b895e5c084e36bea0e0bd5769559bb
jyoung0930 <jaronyoung1@gmail.com>
2023-01-17 15:43:10 -0600

    Function Cleanup
    made the function easier to read.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header({ isDark, setDarkMode }) {
   return (
     <div className="header">
       <h1 className="title">Devfinder</h1>
-      <div className="mode" style={style} onClick={() => setDarkMode()}>
+      <div className="mode" style={style} onClick={setDarkMode}>
         <h2>{isDark ? "Light" : "Dark"}</h2>
         <img src={isDark ? sun : moon} alt="icon" />
       </div>
